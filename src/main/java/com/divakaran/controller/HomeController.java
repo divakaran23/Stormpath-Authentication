1ce6f7a806beb350f2ae93cb627dec00cf76112f
Divakaran Jeyachandran <divakaran2392@gmail.com>
2017-01-09 18:49:16 -0800

    Basic spring boot with Stormpath connection

--- src/main/java/com/divakaran/controller/HomeController.java
+++ src/main/java/com/divakaran/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.divakaran.controller;
 
+import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.servlet.application.ApplicationResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Divakaran Jeyachandran
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld() {
+    public String helloWorld(HttpServletRequest req) {
+        Application app = ApplicationResolver.INSTANCE.getApplication(req);
+        System.out.println("Hello, " + app.getName());
         return "index";
     }
 }
--- src/main/java/com/divakaran/security/SpringWebSecurity.java
+++ src/main/java/com/divakaran/security/SpringWebSecurity.java
@@ -14,6 +14,6 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.apply(stormpath());
+        //http.apply(stormpath());
     }
 }
