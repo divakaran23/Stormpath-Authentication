f4077a6c68e4db92dfee94405cff0c108d827bf7
Divakaran Jeyachandran <divakaran2392@gmail.com>
2017-01-09 20:26:57 -0800

    Adding group authorization

--- src/main/java/com/divakaran/controller/HomeController.java
+++ src/main/java/com/divakaran/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.divakaran.controller;
 
+import com.divakaran.service.RestrictedService;
 import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.servlet.account.AccountResolver;
 import com.stormpath.sdk.servlet.application.ApplicationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,10 +18,30 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private RestrictedService restrictedService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloWorld(HttpServletRequest req) {
         Application app = ApplicationResolver.INSTANCE.getApplication(req);
         System.out.println("Hello, " + app.getName());
         return "index";
     }
+
+    @RequestMapping(value = "/restricted", method = RequestMethod.GET)
+    public String restricted(HttpServletRequest request, Model model) {
+        if(AccountResolver.INSTANCE.getAccount(request) != null) {
+            model.addAttribute("accountName", AccountResolver.INSTANCE.getAccount(request).getFullName());
+            return "restricted";
+        }
+        return "redirect:/login";
+    }
+
+    @RequestMapping(value = "api/restrict", method = RequestMethod.GET)
+    public String getApi(HttpServletRequest request, Model model) {
+        String message = restrictedService.validateUser(AccountResolver.INSTANCE.getAccount(request));
+        model.addAttribute("message", message);
+        return "hello";
+    }
+
 }
---
+++ src/main/java/com/divakaran/service/RestrictedService.java
@@ -0,0 +1,21 @@
+package com.divakaran.service;
+
+import com.stormpath.sdk.account.Account;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Divakaran Jeyachandran - djeyachandran@expedia.com
+ */
+@Service
+public class RestrictedService {
+
+    private static final String MY_GROUP = "https://api.stormpath.com/v1/groups/174vbJOGSi0pRnWZvBCzHP";
+
+    @PreAuthorize("hasAuthority('" + MY_GROUP + "')")
+    public String validateUser(Account account) {
+        String message = "Hello, " + account.getFullName() +
+                ". You have been given access to this page";
+        return message;
+    }
+}
