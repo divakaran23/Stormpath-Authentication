961a0a1767d7a4596984e01d02d190ecaa0fed82
Divakaran Jeyachandran <divakaran2392@gmail.com>
2017-01-22 13:47:51 -0800

    Adding restricted persmissions to an endpoint

--- src/main/java/com/divakaran/service/RestrictedService.java
+++ src/main/java/com/divakaran/service/RestrictedService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class RestrictedService {
 
-    @PreAuthorize("hasAuthority(@groups.ADMIN)")
+    @PreAuthorize("hasAuthority(@groups.ADMIN) and hasPermission('say', 'hello')")
     public String validateUser(Account account) {
         String message = "Hello, " + account.getFullName() +
                 ". You have been given access to this page";
